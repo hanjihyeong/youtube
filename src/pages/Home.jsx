79e5abe069b51fb82936ad47c30886f64d1e59c8
Jihyeong Han <hjh000504@naver.com>
2023-11-02 16:59:10 +0900

    페이지 SEO 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Main from "../components/section/Main";
 
 const Home = () => {
-  return <Main>Home</Main>;
+  return (
+    <Main title="유튜브 채널" description="유튜브 채널 페이지입니다.">
+      Home
+    </Main>
+  );
 };
 
 export default Home;
