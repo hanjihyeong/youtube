448a08335223dab60b1597734d5c1949cfe96912
Jihyeong Han <hjh000504@naver.com>
2023-11-03 15:49:58 +0900

    버그 고치기

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Logo = () => {
+const Logo = ({ toggleMenu }) => {
   return (
     <h1 className="header__logo">
-        <a href="/">
-          <em aria-hidden="true"></em>
-          <span>
-            Scott
-            <br />
-            YOUTUBE
-          </span>
-        </a>
-      </h1>
-  )
-}
+      <Link to="/">
+        <em aria-hidden="true" onClick={toggleMenu}></em>
+        <span>
+          Scott
+          <br />
+          YOUTUBE
+        </span>
+      </Link>
+    </h1>
+  );
+};
 
-export default Logo
+export default Logo;
--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -4,19 +4,20 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import Header from "./Header";
 import Footer from "./Footer";
 import Search from "./Search";
+import ScrollTo from "../../utils/scrollTo"; // 추가
 
 const Main = (props) => {
   return (
     <HelmetProvider>
+      <ScrollTo /> 
       <Helmet
-        titleTemplate="%s | Hans Youtube"
-        defaultTitle="Hans Youtube"
+        titleTemplate="%s | Webs Youtube"
+        defaultTitle="Webs Youtube"
         defer={false}
       >
         {props.title && <title>{props.title}</title>}
         <meta name="description" content={props.description} />
       </Helmet>
-
       <Header />
       <main id="main" role="main">
         <Search />
